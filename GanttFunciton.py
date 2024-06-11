feb69aef4289b3b00d0ed518f0ad6ab184de9974
Scott Hendricks <scott.hendricks1568@gmail.com>
2024-06-10 21:04:31 -0500

    first commit

---
+++ GanttFunciton.py
@@ -0,0 +1,166 @@
+import sys
+import pandas as pd
+import numpy as np
+import pymsgbox
+from datetime import datetime
+import math
+from datetime import timedelta
+
+dayName = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+# Data validation using ymsgbox
+def alertSheet(ws):
+    """ ws is worksheet name.
+        Return message box if ws name is not as required."""
+    #Required Worksheet is existing?
+    pymsgbox.alert(text= "Worksheet " + ws + " Does not exist", title= 'worksheet' + ws)
+    sys.exit()
+
+def alertMatch(tobeMatched, base, atitle):
+    #Compare a list to another list. This is used to ensure that worksheet columns index are correcty described
+    unmatch = np.setdiff1d(tobeMatched, base)
+    # unmatch = [item for item in tobeMatched if item not in base]
+    if len(unmatch) > 0:
+        unmatch = ', '.join([str(x) for x in unmatch])
+        pymsgbox.alert(text= unmatch + ' are to be corrected.', title = atitle)
+        sys.exit()
+
+def alertDuplicate(sr):
+    #sr should consit of elements which must be unique to each other
+    dups = sr[sr.duplicated()]
+    if len(dups) > 0:
+        dups = ', '.join([str(x) for x in dups])
+        pymsgbox.alert(text= dups + ' are duplicated.', title = 'To be unique')
+        sys.exit()
+def alertPeriodBlank(df):
+    #severy activity should be defined with period
+    acts = [df['Activity'][i] for i in range(len(df['Activity'])) if df['Period'].isnull()[i] == True] # == ['nan]] #['nan'] ]
+    if len(acts) > 0:
+        acts = ', '.join([str(x) for x in acts])
+        pymsgbox.alert(text= acts + ' period is blank.', title = 'Activites with blank period')
+        sys.exit()
+def alertDataEntry(df):
+    acts = [df['Activity'][i] for i in range(len(df['Activity'])) if df['Predecessor'].isnull()[i] == True & df['Start']isnull()[i] == True ] # == ['nan]] #['nan'] ]
+    if len(acts) > 0:
+        acts = ', '.join([str(x) for x in acts])
+        pymsgbox.alert(text= acts + 'start date are not specified for ' + acts + ' without predecessors involved.', title = 'Activites without predecessors')
+        sys.exit()
+
+def remove_space(list):
+    """Assumes list includes nested sublist
+        Returns 2D list without white space for each element"""
+    for i in range(len(list)):
+        for j in range(len(list[i])):
+            list[i][j] = list[i][j].strip()
+    return list
+
+def match_pred(preds, match, option):
+    """Assume preds is predecessors list, match is the list to be matched with predecessor
+        option is either 'Relationship' or 'Time lag'
+        return matching list corresponding to predecessor"""
+    match_pred = []
+    for i in range(len(preds)):
+        if preds[i] != 'nan':
+            cols = []
+            if len(preds[i]) == len(match[i]):
+                for j in range(len(preds[i])):
+                    if match[i][j] == 'nan':
+                    #if match[i][j].isnull() == True: # 'nan':
+                        if option == 'Relationship':
+                            cols.append('fs')
+                        else:
+                            cols.append(0)
+                    else:
+                        cols.append(match[i][j])
+                match_pred.append(cols)
+            else:
+                for j in range(len(preds[i])):
+                    if option == 'Relationship':
+                        cols.append('fs')
+                    else:
+                        cols.append(0)
+                match_pred.append(cols)
+    return match_pred
+
+def endDate(start, period, holidays, nonworkdays, included):
+    """Assumes start is the cmmencing date for the end date calculation, period interger
+        holidays the list of project holidays, nonworkdays the list of non working weekdays.
+        included being boolean whether start date itself is included as part of period.
+        Return start date (working day) and end date"""
+    if period < 0: # define whether incremental of decremental
+        d = -1
+    else:
+        d = 1
+    period = abs(period)
+    if included:
+        # Actual start date should be weekday, no weekend date or not holidays
+        while dayName[start.weekday()] in nonworkdays or start in holidays:
+            start += timedelta(days = d)
+        # Calculate end date
+        endDate = start
+        if period == 0 or period == 1:
+            return start, endDate
+        else:
+            i= 1
+            while i < period:
+                endDate += timedelta(days = d)
+                if dayName[endDate.weekday()] not in nonworkdays and endDate not in holidays:
+                    i += 1
+            return start, endDate
+    else:
+        # compute the ending working date with the start date excluded from period
+        # This is used to evealuate to the activity start date from the predecessors finish dates for forward pass analysis
+        # activity end date from the successors' start date for backward anlaysis, and time lag
+        endDate = start
+        if period == 0:
+            return start, endDate
+        else:
+            i = 0 
+            while i < period:
+                endDate += timedelta(days = d)
+                # endDate = endDate + timedelta(days = d)
+                if dayName[endDate.weekday()] not in nonworkdays and endDate not in holidays:
+                    i += 1
+            return start, endDate
+
+def pslist(activities, predecessors, periods, relations, lags):
+    """
+    Assumes activites is the list of activites, predecessors the list of the activity predecessors
+    periods the list of activbity periods, relations the list of activity relationship,
+    lags the list of activity time lags
+    Return the list of every predecessor-successor along with FS equivalent time lag to faclitate
+    Return the list of every predecessore-seccessor along with FS equivalent time lag to factlitate
+    forward and baward pass calculation, predecessor and successor's corresponding activity indes used for connector
+    """
+    ps = pd.DataFrame(columns= ['Predecessor','Successor', 'Pred duration','Succ duration','Timelag fs','relation', 'lag','predid', 'succid'])
+    n = 0
+    for i in range(len(predecessors[i])):
+        if predecessors[i] != ['nan']
+            for j in range(len(predecessors[i])):
+                pred = predecessors[i][j]
+                succ = activities[i]
+                if pred == succ:
+                    pymsgbox.alert(text= 'Activity, ' + succ + ' should not take itself as its predecessor, ' + pred, title='Prededecessor')
+                    sys.exit()
+                predPeriod = periods.loc[activities == pred].values[0]
+                succPeriod = periods[i]
+                relation = relations[i][j]
+                lag = int(lags[i][j])
+                predId = activities.loc[activities == pred].index.values[0]
+                succId = i
+                if predId > succId:
+                    pymsgbox.alert(text= 'Activity, ' + pred + 'needs to be placed vefore ' + succ, title = 'Activity Sequence')
+                    sys.exit()
+                if relation == 'fs':
+                    lagfs = lag
+                elif relation == 'ss':
+                    lagfs = (lag - predPeriod)
+                elif relation == 'ff':
+                    lagfs = lag=succPeriod
+                elif relation == 'sf':
+                    lagfs = (lag-predPeriod + succPeriod)
+                else:
+                    lagfs = lag
+                ps.loc[n] = [pred, succ, predPeriod, succPeriod, int(lagfs), relation, lag, predId, succId]
+                n += 1
+    return ps    
---
+++ Ganttwoclass.py
@@ -0,0 +1,259 @@
+import numpy as np
+import pandas as pd
+import xlwings as xw
+from GanttFunciton import *
+from datetime import timedelta
+from datetime import date
+from calendar import monthrange
+from xlwings.utils import rgb_to_int
+
+dayName_short = ("M", "T", "W","T","F","S","S")
+months = ("Jan","Feb", 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+chart_startcol = int(12)
+chart_startrow = int(4)
+chart_columnwidth = 1.0 # unit in point
+chart_rowheight = 20 # unit in point and dfault is 15 points
+
+# API options and settings
+pd.set_option('expand_frame_repr', False)
+pd.options.mode.chained_assignment = None
+
+# Establish connection to Excel workbook using xlwings
+# # option 1 - when running python from Excel using caller() method
+# wb = xw.Book.caller()
+# filename = wb.fullname
+
+#option 2 - when running directly in the Python, giving Excel file name
+filename = 'Ganttwoclass.xlsm'
+wb = xw.Book(filename)
+
+# Identify sheet name
+wsname1 = 'cpa'
+wsname2 = 'Calendar'
+
+# check whether  worksheet named 'cpa' exists or not
+try:
+    ws1 = wb.sheets[wsname1]
+except:
+    alertSheet(wsname1)
+try:
+    ws2 = wb.sheets[wsname2]
+except:
+    alertSheet(wsname2)
+
+# Read Excel dat
+# 1) Schedule data, read and store in a pandas DataFrame
+schData = pd.read_excel(io = filename, sheet_name= wsname1, index_col=None, usecols = 'A:K', skiprows = 2,)
+print(schData)
+size = len(schData)
+# 2) Calendar data, read and store in a pandas DataFrame
+calData = pd.read_csv(filename,wsname2, index_col=None, usecols= 'A:D',)
+
+
+# Validate Excel data
+# 1) Validate schedule data
+colsSch = ['Activity', 'Description', 'Predecessor', 'Period', 'Start', 'Finish', 'Late Start', 'Late Finish', 'Float', 'Relationship', 'Time lag']
+userColsSch = schData.columns.tolist() # array converted to list
+alertMatch(userColsSch, colsSch, 'Schedule data columns unmatch') # check whether spread sheet columns are correctlyu described as coded in program
+alertDuplicate(schData['Activity']) # Activiteies defined are not to be duplicated
+alertPeriodBlank(schData[['Activity','Period']])# Activites without periods found
+alertDataEntry(schData[['Activity','Predecessor', 'Start']]) # alertBlank(schData['redecessor'], schData['Start'])
+# 2) Validate calendar data
+colsCal = ['type', 'Date', 'Hours', 'Note']
+userColsCal = calData.columns.tolist()
+alertMatch(userColsCal, colsCal, 'Project calendar data columns unmatch')
+
+# data transformation
+# (1) convert Excel cell contents (desinged for multiple elements and separate by comma) into a list: applicable to predecessors, relationships and time lags
+# (1-1) split
+# Using for in loop
+# predecessors = []
+# for preds in schData['Predecessor'].apply(str):
+#   pred = preds.split(',')
+#   predecessors.append(pred)
+#Based on list comprehension
+predecessors = [preds.split(',') for preds in schData['Predecessor'].apply(str)]
+
+#(1-2) remove space
+# Based on a custom function (remove_space with iterations)
+# predecessors = remove_space(predecessors)
+# Based on nested list comprehesion
+predecessors = [[x.strip() for x in [preds.split(',') for preds in schData['Predecessor'].apply(str)][i]] for i in range(size)]
+relations = [[x.strip() for x in [rels.split(',') for rels in schData['Relationship'].apply(str)][i]] for i in range(size)]
+lags = [[x.strip() for x in [timelags.split(',') for timelags in schData['Time lag'].apply(str)][i]] for i in range(size)]
+
+# (1-3) Realationship and time lag data to match with predecessors
+relations = match_pred(predecessors, relations ,'Relationship')
+lags = match_pred(predecessors, lags, 'Time lag')
+
+#(2) Sort out Holidays and non work days into corresponding list
+holidays = calData['Date'].loc[calData['Type'] == 'Holiday'].values
+nonworkdays = calData['Note'].loc[(calData['Type'] == 'Weekday') & (calData['Hours'] == 0)].values
+
+#(3) Create predecessor - seccessor list, each one to one relation
+# Create the list of 
+
+
+ps = pslist(schData['Activity'], predecessors, schData['Period'], relations, lags)
+# predecessors should refer to defined activity only
+alertReference(ps['Predecessor'], schData['Activity'])
+
+# (4) Find out project start activity and project end activity
+p = set(ps['Predecessor'])
+s = set(ps['Successor'])
+startActs = p - s
+endActs = s - p
+
+# forward pass analysis for early start and early finish date
+# # with nested loop
+# for x in startActs:
+#     for i in range(len(schData)):
+#         if x == schData['Activity'][i]:
+#             earlySch=endDate(schData['Start'][i], schData['Period'][i], holidays, nonworkdays, included=True)
+#             schData['Start'][i] = earlySch[0]
+#             schData['Finish'][i] = earlySch[1]
+#             # print(type(schData['Finish']))
+# withou nested loop
+for x in startActs:
+    i = schData['Activity'].loc[schData['Activity'] == x].index.values[0]
+    earlySch = endDate(schData['Start'][i], schData['Period'][i], holidays, nonworkdays, included=True)
+    schData['Start'][i] = earlySch[0]
+    schData['Finish'][i] = earlySch[1]
+for i in range(len(schData)):
+    if predecessors[i] != ['nan']:
+        preds = predecessors[i]
+        predsEnds = []
+        for j in range(len(preds)):
+            predsLag = ps['Time lag fs'].loc[(ps['Predecessor'] == preds[j]) & (ps['Successor'] == schData['Activity'][i])].values[0]
+            predsEnd = pd.Timestamp(schData['Finish'].loc[schData['Activity'] == preds[j]].values[0])
+            predsEnd.append(endDate(predsEnd, predsLag, holidays, nonworkdays, False)[1])
+        schData['Start'][i] = endDate(max(predsEnd), 1, holidays, nonworkdays, False)[1]
+        schData['Finish'][i] = endDate(schData['Start'][i], schData['Period'][i], holidays, nonworkdays, True)[1]
+
+# transverse list in the reverse order
+# # method 1 based on list slicing
+# for act in schData['Activity'][::-1]:
+# # method 2 based on the range(start, stop, step) functio
+for i in range(len(schData)-1, -1, -1):
+    # Find out the activity's successor(s):
+    succs = ps['Successor'].loc[ps['Predecessor'] == schData['Activity'][i]].tolist()
+    if len(succs) != 0:
+        succStarts = []
+        for j in range(len(succs)):
+            succLag = ps['Time lag fs'].loc[(ps['Successor'] == succs[j]) & (ps['Predecessor'] == schData['Activity'][i])].values[0]
+            succStart = pd.Timestamp(schData['Late Start'].loc[schData['Activity'] == succs[j]].values[0])
+            succStarts.append(endDate(succStart, -succLag, holidays, nonworkdays, False)[1])
+        schData['Late Finish'][i] = endDate(min(succStarts), -1, holidays, nonworkdays, False)[1]
+        schData['Late Start'][i] = endDate(schData['Late Finish'][i],-schData['Period'][i], holidays, nonworkdays, True)[1]
+
+# Float calculation
+schData['Float'] = schData['Late Finish'] - schData['Finish']
+
+# Complte Schedule data based on Critical path analysis result
+schData['Start'] = [x.date() for x in schData['Start']]
+schData['Finish'] = [x.date() for x in schData['Finish']]
+schData['Late Start'] = [x.date() for x in schData['Late Start']]
+schData['Late Finish'] = [x.date() for x in schData['Late Finish']]
+schData['Float'] = [x.date() for x in schData['Float']] # time delta is to be coverted to days
+
+# write to the worksheet, the schData DataFrame with analysis done above
+calculatedData = pd.DataFrame(schData, columns=['Start', 'Finish', 'Late Start','Late Finish', 'Float'])
+wb.sheets[wsname1].range('E3').options(index=False).value = calculatedData # write to worksheet
+
+# Three(3) tiers schedule timeplate consisting of year + month, date, weekdays
+# (1) clean the existing chart area from chart starting column 12 to the existing last column including deleting shapes (bars and connectores)
+last_column = ws1.range(3,1).end('right').last_cell.column
+ws1[:, chart_startcol-1:last_column + 1].delete()
+ws1[chart_startrow-1:chart_startrow + size, :].api.RowHeight = chart_rowheight
+ws1[chart_startrow-1:chart_startrow + size, :].api.VerticalAlignment = xw.constants.VAlign.xlVAlignCenter #vertical alignment middle
+
+# (2) three(3) tier template data (covering from 1st day of the starting month till end of the ending month)
+dayone = min(schData['Start'])
+dayend = max(schData['Late Finish'])
+dayone = date(dayone.year, dayone.month, 1)
+dayend = date(dayend.year, dayend.month+1, 1) - timedelta(days=1)
+top = [months[(dayone + timedelta(days = i)).month-1] + '-' + str((dayone + timedelta(days=i)).year) for i in range((dayend - dayone).days + 1)] # month - year
+mid = [dayName_short[(dayone + timedelta(days = i)).weekday()] for i in range((dayend - dayone).days + 1)] # weekday in short
+bottom = [(dayone + timedelta(days = i)).day for i in range((dayend-dayone).days + 1)]# date
+templateData = pd.DataFrame(list(zip(top,mid,bottom))).transpose()
+ws1.range('L1').options(index=False, header = False).value = templateData
+# (3) top layer format requiring merge cells extending each associated onths's day
+month_years = (dayend.month - dayone.month + 1) +12*(dayend.year - dayone.year)
+colmerges = [monthrange(dayone.year, dayone.month + i) [i] for i in range(month_years)]
+for i in range(len(colmerges)):
+    if i == 0:
+        mstart = chart_startcol
+        mend = mstart +colmerges[i] - 1
+        ws1.range((1, mstart), (1, mend)).merge()
+    else:
+        mstart = mend + 1
+        mend = mstart + colmerges[i] - 1
+        ws1.range((1, mstart), (1, mend)).merge()
+
+# (4) Highlight nonworkdays
+holidays = [x.date() for x in holidays]
+holCols = [i + chart_startcol for i in range((dayend - dayone).days) if dayone +timedelta(days = i) in holidays]
+nonworkdays = [i + chart_startcol for i in range((dayend - dayone).days) if dayone + timedelta(days = i) in holidays or dayName[(dayone + timedelta(days=i)).weekday()] in nonworkdays]
+
+for i in range(len(nonworkdays)):
+    ws1.range(3, nonworkdays[i]).color = (230,175,220)
+
+# create schedule barcharts with relationship connectors
+# (1) early schedule bars
+startcol = (schData['Start'] - dayone).dt.days + chart_startcol # schData['Start'] is series, schdata['start'] - dayone is series
+chart_columnwidth_point = ws1.cells(chart_startrow,chart_startcol).width
+barlength = ((schData['Finish'] - schData['Start']).dt.days +1) * chart_columnwidth_point - 0.2 * chart_columnwidth_point # number of columns per period
+bars = [None] * len(schData) # Store each shapes instance for drawing connectors
+for i in range (len(schData)):
+    left = ws1.range(chart_startrow + i, startcol[i]).left + 0.1 * chart_columnwidth
+    top = ws1.range(chart_startrow + i, startcol[i]).top + 0.1 * chart_rowheight
+    width = barlength[i]
+    height = chart_rowheight*0.35
+    bars[i] = ws1.api.Shapes.AddShape(1, left, top, width, height)
+    if schData['Float'][i] == 0:
+        bars[i].Fill.ForeColor.RGB = rgb_to_int((255,51,204))
+    else:
+        bars[i].Fill.ForeColor.RGB = rgb_to_int((80,58,244))
+
+# (2) produce connectors to show schedule activitties relationship based on early schedule bars
+for i in range(len(ps)):
+    ipred = ps['predid'][i]
+    isucc = ps['succid'][i]
+    if ipred != 'None':
+        if ps['relation'][i] == 'fs':
+            ibegin = 4
+            iend = 2
+        elif ps['relation'][i] == 'ss':
+            ibegin = 2
+            iend = 2           
+        elif ps['relation'][i] == 'ff':
+            ibegin = 4
+            iend = 4  
+        elif ps['relation'][i] == 'sf':
+            ibegin = 2
+            iend = 4  
+        connector = ws1.api.Shapes.AddConnector(2, left, top, 1,1) # connector type 2 for elbow connector
+        connector.Line.EndArrowheadStyle = 4 # end arrow head style 4 for stealth bomber
+        connector.Line.ForeColor.RGB = rgb_to_int((0,0,0))
+        connector.ConnectorFormat.BeginConnect(bars[ipred], ibegin)
+        connector.ConnectorFormat.EndConnect(bars[isucc], iend)
+
+# (3) Late schedule bars
+startcol_late = (schData['Late Start'] - dayone).dt.days + chart_startcol
+barlength_late = ((schData['Late Finish'] - schData['Late Start']).dt.days + 1) * chart_columnwidth_point - 0.2 * chart_columnwidth_point
+for i in range(len(schData)):
+    left = ws1.range(chart_startrow + i, startcol_late[i]).left + 0.1 * chart_columnwidth
+    top = ws1.range(chart_startrow + i, startcol_late[i]).top + 0.55 * chart_rowheight
+    width = barlength_late[i]
+    height = chart_rowheight*0.35
+    bar_late = ws1.api.Shapes.AddShape(1, left, top, width, height)
+    bar_late.Fill.ForeColor.RGB = rgb_to_int((175,175,175))
+    bar_late.ZOrder(1) #ZOrdercmd is parameter, 1 for SendtoBack, this is for the purpose of connectoers not ito be below bars
+
+# schedule chart area formatting - column width, borders (thin and light grey), orientation 90 degree
+last_column = ws1.range(3,1).end('right').last_cell.column # check last column number for very inital pgoram run
+ws1.range((chart_startrow, chart_startcol), (chart_startrow + len(schData), (dayend-dayone).days + chart_startcol)).column_width = chart_columnwidth
+ws1.range((1, chart_startcol), (chart_startrow + len(schData) -1, last_column)).api.Borders.Weight = 2 # thin
+ws1.range((1, chart_startcol), (chart_startrow + len(schData) -1, last_column)).api.Borders.Color = rgb_to_int((166,166,166)) # light grey
+
+
